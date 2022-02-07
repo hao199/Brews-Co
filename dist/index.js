d08a763ede04e79141bab513f57863343f7c28af
hao199 <haoze199@gmail.com>
2022-02-08 03:45:22 +0800

    Added html and use three.js library for Homepage

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,70 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
-const canvas = document.querySelector('canvas.webgl')
-const scene = new THREE.Scene()
 
-const geometry = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({
-    color:0x00ff00
-})
+//Set up
+const scene = new THREE.Scene();
 
-const mesh = new THREE.Mesh(geometry,material)
-scene.add(mesh)
-
-const sizes = {
-    width:window.innerWidth,
-    height:window.innerHeight
-}
-
-const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height,0.1,100)
-camera.position.set(0,1,2)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
+  canvas: document.querySelector('#bg'),
+});
 
-renderer.setSize(sizes.width,sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
-renderer.shadowMap.enabled = true
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
 
-function animate(){
-    requestAnimationFrame(animate)
-    renderer.render(scene,camera)
+renderer.render(scene, camera);
+
+//Shape
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+
+//Light
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+//Helpers
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200,50);
+scene.add(lightHelper, gridHelper)
+
+//Stars
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+  
+    const [x, y, z] = Array(3)
+      .fill()
+      .map(() => THREE.MathUtils.randFloatSpread(100));
+  
+    star.position.set(x, y, z);
+    scene.add(star);
+  }
+  
+  Array(200).fill().forEach(addStar);
+
+//Avatar
+
+
+//Animate Loop
+function animate() {
+    requestAnimationFrame(animate);
+
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+
+    renderer.render(scene, camera);
 }
 
 animate()
