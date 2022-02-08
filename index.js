1aa70b8a339cdd6541c3b59d7dd3c3852dfef0fe
hao199 <haoze199@gmail.com>
2022-02-09 02:09:01 +0800

    Updates

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
+
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
