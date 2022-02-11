d950cb73736e64c9159be013b273f69e2e2acccf
hao199 <haoze199@gmail.com>
2022-02-11 17:04:28 +0800

    Account creation js done

--- dist/account.js
+++ dist/account.js
@@ -1,35 +1,42 @@
-var jsondata = {"field1": "xyz","field2": "abc"};
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://brewco-662a.restdb.io/rest/account",
-  "method": "POST",
-  "headers": {
-    "content-type": "application/json",
-    "x-apikey": "62057be51b941c73ff397a58",
-    "cache-control": "no-cache"
-  },
-  "processData": false,
-  "data": JSON.stringify(jsondata)
-}
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
 
+//Post
 $("#signup-btn").click(function(){
+  console.log('we here');
     var username = $("#username").val();
     var email = $("#email").val();
     var password = $("#password").val();
+    var account = {"username": username,"email": email,"password": password};
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://brewco-662a.restdb.io/rest/account",
+      "method": "POST",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": "62057be51b941c73ff397a58",
+        "cache-control": "no-cache"
+      },
+      "processData": false,
+      "data": JSON.stringify(account)
+    }
 
-    $.post("https://brewco-662a.restdb.io/rest/account",{
-        username:username,
-        email:email,
-        password:password
-    },function(response){
-        $("#response").html("<div class='alert alert-success'>"+response.message+"</div>")
-        clearField();
-    })
+    $.ajax(settings).done(function (response) {
+      $("#response").html("<div class='alert alert-success'>Account successfully created</div>");
+      clearField();
+    });
+
+    // $.done("https://brewco-662a.restdb.io/rest/account",{
+    //     username:username,
+    //     email:email,
+    //     password:password
+    // },function(response){
+    //     $("#response").html("<div class='alert alert-success'>"+response.message+"</div>")
+    //     clearField();
+    // })
 })
 
 function clearField(){
